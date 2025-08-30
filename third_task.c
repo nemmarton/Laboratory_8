3ca9f74e867bc25dc05289fa47213f3af3b5b460
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-08-30 12:08:03 +0200

    Update third_task.c

--- third_task.c
+++ third_task.c
@@ -1,6 +1,6 @@
 /* Task description:
-
-
+Write a program, that first asks the user the number of real values to read; then it reads the values into a dynamically allocated array. 
+At the end the program should print the elements of the array backwards, and release the allocated memory!
 */
 
 
