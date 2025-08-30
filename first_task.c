12fc2ee4cef1e1bc6c12d0539dbf195df173d6f2
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-08-30 12:06:21 +0200

    Update first_task.c

--- first_task.c
+++ first_task.c
@@ -1,5 +1,12 @@
 /* Task description:
+Write a program that asks the user to enter a word, stores it in a string, and prints it letter by letter vertically. For example if you give „Word”, the result should be:
 
+W
+o
+r
+d
+
+You can assume that the word is never longer than 99 characters.
 
 */
 
