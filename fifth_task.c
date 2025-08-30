fd4c31c529e4535fb05af2b26bad458e71c37f94
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-08-30 12:14:55 +0200

    Update fifth_task.c

--- fifth_task.c
+++ fifth_task.c
@@ -1,5 +1,7 @@
 /* Task description:
+Write a program that opens a text file and breaks all lines wider than 20 charaters. The output should be written to "output2.txt".
 
+To test the program, use lorem.txt! Check the result!
 
 */
 
